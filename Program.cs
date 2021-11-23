8a41ca766002da1be02883db5f115353c652154e
Alex Rolfe <alex.rolfe27@gmail.com>
2021-11-23 05:32:42 -0600

    parses input into basic MD template and outputs new file

--- Program.cs
+++ Program.cs
@@ -8,27 +8,53 @@ namespace csv_to_md
     {
         static void Main(string[] args)
         {            
-            string filePath = args[1];           
-            string outputPath = Environment.CurrentDirectory + "/newFile.md";
-
+            string filePath = args[1]; 
+            string outputPath = $"{Environment.CurrentDirectory}/{args[2]}.md";
+            string finalText = "";
+            
+            Console.WriteLine(outputPath);
             using (TextFieldParser parser = new TextFieldParser(filePath)) 
             {
                 parser.TextFieldType = FieldType.Delimited;
                 parser.SetDelimiters(",");
+
+                SkipLine(parser);
+
                 while (!parser.EndOfData) 
                 {
-                    string[] fields = parser.ReadFields();
-                    foreach (string field in fields) 
+                    string[] row = parser.ReadFields();
+                    
+                    for (int i = 0; i < row.Length; i++)
                     {
-                        Console.WriteLine(field);
+                        var headerSize = i == 0 ? "##" : "###";
+                        finalText += CreateTemplateSubsection(row[i], headerSize);
                     }
                 }
-            }
+            }            
 
             if (!File.Exists(outputPath))
-            {
-                File.Create(outputPath);
+            {                
+                File.WriteAllText(outputPath, finalText);
             }
         }
+
+        private static string CreateTemplateSubsection(string cell, string headerSize)
+        {
+            // this is ugly and brittle
+            return 
+            @$"{headerSize} {cell}
+
+";
+        }
+
+        private static void SkipLine(TextFieldParser parser)
+        {
+            if (!parser.EndOfData) 
+            {
+                parser.ReadLine();
+            }
+
+        }
+
     }
 }
---
+++ newFile.md
@@ -0,0 +1,16 @@
+## A Streetcar Named Desire
+
+### Tennessee Williams
+
+### Guy gets burned real bad. Yells, "Stella!"
+
+### You can pass on this one.
+
+## The Catcher in the Rye
+
+### JD Salinger
+
+### Kid feels overwhelmed by the weight of it all.
+
+### Pretty good, but hits different as you get farther from age 15
+
