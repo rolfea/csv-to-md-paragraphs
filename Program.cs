8317b10eca5ebe98b101ab63350524b802edef90
Alex Rolfe <alex.rolfe27@gmail.com>
2021-11-22 20:27:03 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualBasic.FileIO;
+
+namespace csv_to_md
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = args[1];                        
+            using (TextFieldParser parser = new TextFieldParser(filePath)) 
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                while (!parser.EndOfData) 
+                {
+                    string[] fields = parser.ReadFields();
+                    foreach (string field in fields) 
+                    {
+                        Console.WriteLine(field);
+                    }
+                }
+            }
+        }
+    }
+}
